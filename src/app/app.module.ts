7da12a8ac7a2c185558b8768123666f907eb84b5
anantjain88 <anant@creolestudios.com>
2019-10-24 12:41:54 +0530

    user login completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { ListUserComponent } from './user/list-user/list-user.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
 import { LoginComponent } from './login/login.component';
-// import { AddUserComponent } from './user/add-user/add-user.component';
-// import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
 import {routing} from "./app-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
-// import {ApiService} from "./service/api.service";
-// import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ApiService} from "./service/api.service";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 // import {TokenInterceptor} from "./core/interceptor";
 
 @NgModule({
@@ -24,9 +24,9 @@ import {ReactiveFormsModule} from "@angular/forms";
     BrowserModule,
     routing,
     ReactiveFormsModule,
-    // HttpClientModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
